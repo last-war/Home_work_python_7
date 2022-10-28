c88f795432490801653dc4e1bb8c20d4f9f557ca
last-war <andrey.maybrodskyy@gmail.com>
2022-10-28 20:38:16 +0300

    working version

--- clean_folder/clean_folder/my_sort.py
+++ clean_folder/clean_folder/my_sort.py
@@ -1,15 +1,30 @@
-from audioop import add
 from os import listdir
 import sys
 from pathlib import Path
 import shutil
-from universal_func import normalize
+import re
 
 IMAGES_SUFFIX = ('JPEG', 'PNG', 'JPG', 'SVG')
 DOCUMENTS_SUFFIX = ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX', 'PPT')
 AUDIO_SUFFIX = ('MP3', 'OGG', 'WAV', 'AMR')
 VIDEO_SUFFIX = ('AVI', 'MP4', 'MOV', 'MKV')
 ARCHIVES_SUFFIX = ('ZIP', 'GZ', 'TAR')
+
+
+def normalize(in_string: str) -> str:
+    """Replace cirilan symbils on latitians and change other to _ (except digit)
+
+    Args:
+        in_string (string):  need re module and use string translate
+    """
+    lation = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+              "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+    trans = {}
+    for c, l in zip("абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ", lation):
+        trans[ord(c)] = l
+        trans[ord(c.upper())] = l.upper()
+
+    return re.sub(r'[^0-9a-zA-Z]', '_', in_string.translate(trans))
 
 
 def main():
